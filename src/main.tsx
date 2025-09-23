28c84d39604f423974e397ddd84b404a1b0e75f4
L1m3Kun <sungku2757@gmail.com>
2025-09-23 15:12:35 +0900

    🎉 Init: project init
    project setting with react & ts (cc. readdy.ai)

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from "react";
+// import "./i18n";
+import "./index.css";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
